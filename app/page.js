8eb7947ffc507d5bac7a7b6f594307a063096ae9
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-08-26 17:15:00 +0500

    done

--- app/page.js
+++ app/page.js
@@ -131,7 +131,7 @@ export default function Home() {
                     marginTop: "2px",
                   }}
                 >
-                  {item}
+                  {item}.
                 </li>
               )
           )}
